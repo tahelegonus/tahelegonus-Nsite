1db1704f3806f544e462c2ed56277450c61d3a85
tahelegonus <telegonuscpt866@gmail.com>
2026-07-17 14:32:11 -0400

    theme_branding.py

---
+++ theme_branding.py
@@ -0,0 +1,14 @@
+#tahelegonus branding 
+from theme_console import console
+
+APP_NAME = "TAHELEGONUS"
+
+VERSION = "1.0.0"
+
+AUTHOR = "tahelegonus"
+
+ENGINE = "TeddyBear"
+
+APP_SUBTITLE = "Windows Security Analysis Framework"
+
+FONT = "slant"
