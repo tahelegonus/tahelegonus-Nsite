b5aa30aea70cd1591ed48d4079a4f53cc3eede34
tahelegonus <telegonuscpt866@gmail.com>
2026-07-17 14:36:08 -0400

    theme_layout.py

---
+++ theme_layout.py
@@ -0,0 +1,31 @@
+
+#shows the overall spacing and sizing of the UI, changing these values
+#and updates the apperance of the enitre application 
+
+
+#application layout --> 
+
+APP_WIDTH = 100          # content width
+PANEL_WIDTH = 80         
+TABLE_WIDTH = 80
+
+#spacing --> 
+
+TOP_MARGIN = 1
+BOTTOM_MARGIN = 1
+
+SECTION_SPACING = 1      # displays blank lines between sections
+PANEL_PADDING = (1, 2)   #  for vertical and horizontal 
+
+#banner 
+
+LOGO_SPACING = 1
+SUBTITLE_SPACING = 1
+
+
+#dashboard
+CARD_PADDING = (1, 2)
+
+#divider 
+
+DIVIDER_CHARACTER = "─"
