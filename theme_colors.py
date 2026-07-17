7edcc60297b7211a67ceb810e847e6535fa4e0ee
tahelegonus <telegonuscpt866@gmail.com>
2026-07-17 14:32:53 -0400

    theme_colors.py

---
+++ theme_colors.py
@@ -0,0 +1,21 @@
+#tahelegonus color pallete
+
+PRIMARY = "bright_magenta"
+
+SECONDARY = "cyan"
+
+TEXT = "white"
+
+SUCCESS = "green"
+
+WARNING = "yellow"
+
+ERROR = "red"
+
+INFO = "bright_blue"
+
+BORDER = "grey50"
+
+SUBTITLE_COLOR = "grey70"
+
+MUTED = "grey62"
